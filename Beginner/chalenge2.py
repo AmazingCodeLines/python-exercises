f68893e3df225f0d48236a12ef26fc14138ce949
AmazingCodeLines <amazingcodelines@gmail.com>
2024-07-07 12:12:26 +0200

    Solution chalenge 2, Leap Year Checker

--- Beginner/chalenge2.py
+++ Beginner/chalenge2.py
@@ -1,12 +1,12 @@
 """
-Challenge Exercise 2: Leap Year Checker
+Challenge Exercise 2: 
 
-Write a Python program that determines whether a given year is a leap year or not. Your program should meet the following requirements:
+Write a Python program that determines whether a given year is a leap year or not. 
+Your program should meet the following requirements:
 
 Input Handling:
 Prompt the user to enter a year.
 Ensure the input is a valid integer.
-Allow the user to type 'exit' to quit the program.
 
 Leap Year Rules:
 A year is a leap year if it is divisible by 4.
@@ -18,26 +18,7 @@ If the input year is not a leap year, print: "<year> is not a leap year."
 If the input is not a valid integer, print: "Please enter a valid integer year.”
 
 Loop:
-The program should continuously prompt the user for a year until the user types ‘exit'.
-
-
-Example Interaction
-
-Enter a year (or type 'exit' to quit): 2000
-2000 is a leap year.
-
-Enter a year (or type 'exit' to quit): 1900
-1900 is not a leap year.
-
-Enter a year (or type 'exit' to quit): abc
-Please enter a valid integer year.
-
-Enter a year (or type 'exit' to quit): 2021
-2021 is not a leap year.
-
-Enter a year (or type 'exit' to quit): exit
-Exiting the program.
-
+The program should continuously prompt the user for a year.
 
 Tips:
 Use a while loop to continuously prompt the user.
@@ -45,3 +26,26 @@ Use try-except or input validation methods to check if the input is an integer.
 Define a function to determine if a year is a leap year for better code organization.
 
 """
+
+print("Welcome to the leap year checker!")
+
+while True:
+    def get_valid_year(prompt):
+        while True:
+            try:
+                return int(input(prompt))
+            except ValueError:
+                print("Invalid Input. Please try again.")
+
+    user_year = get_valid_year("Insert a year: ")
+
+    if user_year % 4 == 0:
+        if user_year % 100 == 0:
+            if user_year % 400 == 0:
+                print(f"{user_year} is a leap year.")
+            else:
+                print(f"{user_year} is not leap year.")
+        else:
+            print(f"{user_year} is a leap year.")
+    else:
+        print(f"{user_year} is not leap year.")
--- Beginner/chalenge7.py
+++ Beginner/chalenge7.py
@@ -1,4 +1,57 @@
+"""
+### Rock, Paper, Scissors Challenge ###
 
+Objective:
+Create a Python program that simulates a Rock, Paper, Scissors game where a user plays against the computer. 
+The program should prompt the user to input their choice, randomly generate the computer's choice, and then determine and display the winner.
+
+
+Instructions:
+
+Define the choices using ASCII art.
+
+User Input:
+
+Prompt the user to enter their choice.
+
+Computer Choice:
+Randomly select the computer's choice.
+
+Determine the Winner:
+Compare user and computer choices to determine the winner.
+
+Display Results:
+Show the choices and the winner.
+
+ASCII:
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+"""
 import random
 
 
@@ -31,12 +84,8 @@ scissors = '''
 '''
 
 possible_options = [rock, paper, scissors]
-
-user_choice = int(input("Make your choice.\n[1] Rock\n[2] Paper\n[3] Scissors\n"))
-
 opponent_choice = random.choice(possible_options)
-
-
+user_choice = int(input("Make your choice.\n[1] Rock\n[2] Paper\n[3] Scissors\n"))
 
 
 if user_choice == 1:
