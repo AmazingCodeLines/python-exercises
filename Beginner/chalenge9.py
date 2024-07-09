a5bb930cb53a9b06b596555b58eb0543c897276d
AmazingCodeLines <amazingcodelines@gmail.com>
2024-07-09 08:01:51 +0200

    chalenge 9

--- Beginner/chalenge9.py
+++ Beginner/chalenge9.py
@@ -34,3 +34,12 @@ Each number/text should be printed on a separate line.
 
 """
 
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")  
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
